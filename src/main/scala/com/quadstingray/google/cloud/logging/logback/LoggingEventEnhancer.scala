bc28d64a39a8f936c808e3d4ffe27cea340ccc0e
travis <6@992cc0715e68a07cf6d407ee1282ed9cdc763f72>
2019-06-19 15:24:23 +0000

    updated site

--- src/main/scala/com/quadstingray/google/cloud/logging/logback/LoggingEventEnhancer.scala
+++
@@ -1,9 +0,0 @@
-package com.quadstingray.google.cloud.logging.logback
-
-import ch.qos.logback.classic.spi.ILoggingEvent
-import com.google.cloud.logging.LogEntry
-
-
-trait LoggingEventEnhancer {
-  def enhanceLogEntry(builder: LogEntry.Builder, e: ILoggingEvent)
-}
